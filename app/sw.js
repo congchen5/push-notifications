2b0f272b958840648c83c41ea540d6d138a58bc1
Cong Chen <congchen5@gmail.com>
2016-09-19 15:59:47 -0700

    Uncommented code that wasn't meant to be commented.

--- app/sw.js
+++ app/sw.js
@@ -35,13 +35,13 @@ self.addEventListener('push', function(event) {
   console.log('Push message received', event);
 
   var title = 'Push message';
-  //event.waitUntil(
+  event.waitUntil(
     self.registration.showNotification(title, {
       body: 'The Message',
       icon: 'images/icon.png',
       tag: 'my-tag'
     });
-  //  }));
+  }));
 });
 
 self.addEventListener('notificationclick', function(event) {
